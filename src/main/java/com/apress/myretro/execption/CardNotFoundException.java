dd2fa6171c7ca4bc003794d589d712550eb9ed64
Ruhanyat-994 <alruhanyat994@gmail.com>
2025-09-21 23:42:53 +0600

    Custom ResponseEntity added in RetroBoardResponseEntityException so that I can generate Json Response while got Exception

---
+++ src/main/java/com/apress/myretro/execption/CardNotFoundException.java
@@ -0,0 +1,16 @@
+package com.apress.myretro.execption;
+
+public class CardNotFoundException extends RuntimeException {
+    public CardNotFoundException() {
+        super("Card Not Found");
+    }
+
+    public CardNotFoundException(String message) {
+        super(String.format("Card Not Found: {} ",message));
+    }
+
+    public CardNotFoundException(String message, Throwable cause){
+        super(String.format("Card Not Found: {}",message),cause);
+    }
+
+}
